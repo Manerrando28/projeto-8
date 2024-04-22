d18b533827af05873d5a876421ee6d16518ce4fb
gabriel <gabrielgleste@gmail.com>
2024-04-22 09:34:15 -0300

    criando o projeto8

---
+++ 7/script.js
@@ -0,0 +1,7 @@
+let p1=prompt("digite seu poder");
+let p2=prompt("digite seu poder");
+let p3=prompt("digite o nome de um vilao");
+let p4=prompt("digite um lugar");
+let msg=document.getElementById("msg");
+
+msg.innerHTML=`<p>ola, ${p1} o seu poder e ${p2} e voce vai enfrentar o (a) ${3} no(em) ${p4}</p>`
